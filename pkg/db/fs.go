739a9bb92c823324a2c378b3a9312d8b2d371423
tycho <garen@tychoish.com>
2026-03-28 16:59:41 -0400

    feat: rebuild database super lazily

--- pkg/db/fs.go
+++ pkg/db/fs.go
@@ -6,7 +6,10 @@ import (
 	"errors"
 	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/tychoish/fun/erc"
 	"github.com/tychoish/fun/irt"
@@ -19,8 +22,6 @@ import (
 //go:embed fasoladb/minutes_schema.sql
 var packaged embed.FS
 
-func getDBpath() string { return filepath.Join(os.TempDir(), "minutes.db") }
-
 func Reset() error {
 	dbPath := getDBpath()
 	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
@@ -31,23 +32,32 @@ func Reset() error {
 
 func Init() error {
 	dbPath := getDBpath()
+	// if the database exists, in /tmp we can just use it as a timesaving measure.
 	if _, err := os.Stat(dbPath); !os.IsNotExist(err) {
-		db, err := sql.Open("sqlite", dbPath)
-		if err != nil {
-			return err
-		}
-		defer db.Close()
+		odemMtime := odemBinaryMtime()
+		dbMtime := odemTempDBMtime()
+		// compare the mtimes; if the binry is compiled and newer than the database; we
+		// should rebuild the database. otherwise the database was built with this binary
+		// and we can just run the idempotent index creation (for the 'go run' case) and use
+		// the database otherwise we fall through and set up the local database
+		if odemMtime.IsZero() || dbMtime.After(odemMtime) {
+			db, err := sql.Open("sqlite", dbPath)
+			if err != nil {
+				return err
+			}
+			defer db.Close()
 
-		setupSql, err := packaged.ReadFile("views.sql")
-		if err != nil {
-			return err
-		}
+			setupSql, err := packaged.ReadFile("views.sql")
+			if err != nil {
+				return err
+			}
 
-		if _, err := db.Exec(string(setupSql)); err != nil {
-			return err
-		}
+			if _, err := db.Exec(string(setupSql)); err != nil {
+				return err
+			}
 
-		return nil
+			return nil
+		}
 	}
 
 	grip.Infoln("setting up the local minutes database:", dbPath)
@@ -91,3 +101,44 @@ func Init() error {
 	grip.Info("database initialized")
 	return ec.Resolve()
 }
+
+func odemBinaryMtime() time.Time {
+	if ex, err := os.Executable(); err == nil && strings.Contains(ex, "go-build") {
+		return time.Time{}
+	} else if stat, err := os.Stat(ex); err == nil {
+		return stat.ModTime()
+	}
+
+	if len(os.Args) == 0 || os.Args[0] == "go" || !strings.HasPrefix(os.Args[0], "odem") {
+		return time.Time{}
+	}
+
+	for p := range irt.RemoveZeros(irt.Args(
+		lookInPath("odem"),
+		lookInPath(os.Args[0]),
+		filepath.Join(pwd(), "odem"),
+		filepath.Join(pwd(), os.Args[0]),
+	)) {
+		stat, err := os.Stat(p)
+		if os.IsNotExist(err) || stat == nil {
+			continue
+		}
+		grip.Noticeln("fallback", p)
+		return stat.ModTime()
+	}
+
+	// well we tried
+	return time.Time{}
+}
+
+func odemTempDBMtime() time.Time {
+	stat, err := os.Stat(getDBpath())
+	if os.IsNotExist(err) || stat == nil {
+		return time.Time{}
+	}
+	return stat.ModTime()
+}
+
+func getDBpath() string             { return filepath.Join(os.TempDir(), "minutes.db") }
+func pwd() string                   { v, _ := os.Getwd(); return v }
+func lookInPath(name string) string { v, _ := exec.LookPath(name); return v }
