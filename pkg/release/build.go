deffe41412d1dc9a947e05bc326e01e0152301ec
tycho <garen@tychoish.com>
2026-04-18 16:11:18 -0400

    feat(release): make release build globally avalible

--- pkg/release/build.go
+++ pkg/release/build.go
@@ -3,12 +3,14 @@ package release
 import (
 	"context"
 	"fmt"
+	"iter"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/tychoish/fun/dt"
 	"github.com/tychoish/fun/erc"
+	"github.com/tychoish/fun/ers"
 	"github.com/tychoish/fun/exc"
 	"github.com/tychoish/fun/fnx"
 	"github.com/tychoish/fun/irt"
@@ -38,123 +40,131 @@ func makeBaseCommand(ctx context.Context) *exc.Command {
 func BuildArtifacts(ctx context.Context) error {
 	conf := odem.GetConfiguration(ctx)
 
-	versionString := Version.Resolve().String()
-	ldFlag := fmt.Sprintf(ldFlagTmpl, versionString, time.Now().Round(time.Millisecond).Format(time.RFC3339))
+	for basepath := range basePathCandidates(conf) {
+		basecmd := makeBaseCommand(ctx).WithDirectory(basepath)
+		versionString := Version.Resolve().String()
+		ldFlag := fmt.Sprintf(ldFlagTmpl, versionString, time.Now().Round(time.Millisecond).Format(time.RFC3339))
 
-	var ec erc.Collector
-	var jobs dt.List[fnx.Worker]
+		var ec erc.Collector
+		var jobs dt.List[fnx.Worker]
 
-	namePart := fmt.Sprintf("%s-v%s", Name, versionString)
-	versionBuildPath := filepath.Join(conf.Build.Path, namePart)
+		namePart := fmt.Sprintf("%s-v%s", Name, versionString)
+		versionBuildPath := filepath.Join(basepath, conf.Build.Path, namePart)
 
-	basecmd := makeBaseCommand(ctx)
-	for build := range irt.Slice(conf.Build.Targets) {
-		buildName := joindash(build.GOOS, build.GOARCH)
-		binaryPath := filepath.Join(versionBuildPath, buildName)
-		if !conf.Runtime.DryRun {
-			ec.Push(mkdirdashp(binaryPath))
-		}
-
-		artifactName := fmt.Sprintf("%s-%s-%s-%s", Name, versionString, build.GOOS, build.GOARCH)
-		var binaryName string
-		if build.GOOS == "windows" {
-			binaryName = joindot(Name, "exe")
-		} else {
-			binaryName = Name
-		}
-
-		binPath := filepath.Join(binaryPath, binaryName)
-
-		steps := []*exc.Command{
-			basecmd.Clone().
-				WithName("go").
-				WithArgs("build", ldFlag, "-o", binPath, "./cmd/odem.go").
-				SetEnvVar("GOOS", build.GOOS).
-				SetEnvVar("GOARCH", build.GOARCH),
-			basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.sha256", binPath, filepath.Join(versionBuildPath, artifactName))),
-		}
-
-		if !conf.Build.DisableCompression {
-			zpath := joindot(binaryPath, "lzma")
+		for build := range irt.Slice(conf.Build.Targets) {
+			buildName := joindash(build.GOOS, build.GOARCH)
+			binaryPath := filepath.Join(versionBuildPath, buildName)
 			if !conf.Runtime.DryRun {
-				ec.Push(mkdirdashp(zpath))
+				ec.Push(mkdirdashp(binaryPath))
 			}
-			zbin := filepath.Join(zpath, binaryName)
 
-			upxcmd := basecmd.Clone().WithName("upx")
-			if build.GOOS == "darwin" {
-				upxcmd.WithArgs("--force-macos", "-q", "--lzma", binPath, "-o", zbin)
+			artifactName := fmt.Sprintf("%s-%s-%s-%s", Name, versionString, build.GOOS, build.GOARCH)
+			var binaryName string
+			if build.GOOS == "windows" {
+				binaryName = joindot(Name, "exe")
 			} else {
-				upxcmd.WithArgs("-q", "--lzma", binPath, "-o", zbin)
-			}
-			steps = append(steps,
-				upxcmd,
-				basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.lzma.sha256", binPath, filepath.Join(versionBuildPath, artifactName))),
-			)
-
-			if build.GOARCH == "386" {
-				bin32 := filepath.Join(versionBuildPath, joinstr(Name, "32"))
-				steps = append(steps,
-					basecmd.Clone().WithName("cp").WithArgs(zbin, bin32),
-					basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.sha256", bin32, bin32)),
-				)
+				binaryName = Name
 			}
 
-			if build.GOOS == "darwin" && build.GOARCH == "arm64" {
-				binapp := filepath.Join(versionBuildPath, joindot(Name, "app"))
-				steps = append(steps,
-					basecmd.Clone().WithName("cp").WithArgs(binPath, binapp),
-					basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.sha256", binapp, binapp)),
-				)
-			}
-		}
+			binPath := filepath.Join(binaryPath, binaryName)
 
-		switch build.GOOS {
-		case "windows":
-			zipball := filepath.Join(versionBuildPath, joindot(artifactName, "zip"))
-			steps = append(steps,
-				basecmd.Clone().WithName("zip").WithArgs("-j", zipball, binPath),
-				basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.sha256", zipball, zipball)),
-			)
-		default:
-			tarball := filepath.Join(versionBuildPath, joindot(artifactName, "tar.gz"))
-			steps = append(steps,
-				basecmd.Clone().WithName("tar").WithArgs("czvf", tarball, "-C", binaryPath, binaryName),
-				basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.sha256", tarball, tarball)),
-			)
-		}
-
-		if conf.Runtime.DryRun {
-			for _, step := range steps {
-				grip.Info(grip.KV("cmd", step.Name).KV("args", step.Args).KV("cmd", step))
+			steps := []*exc.Command{
+				basecmd.Clone().
+					WithName("go").
+					WithArgs("build", ldFlag, "-o", binPath, "./cmd/odem.go").
+					SetEnvVar("GOOS", build.GOOS).
+					SetEnvVar("GOARCH", build.GOARCH),
+				basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.sha256", binPath, filepath.Join(versionBuildPath, artifactName))),
 			}
-			continue
-		} else {
-			jobs.PushBack(func(ctx context.Context) error {
-				for _, step := range steps {
-					if err := step.Run(ctx); err != nil {
-						return err
-					}
+
+			if !conf.Build.DisableCompression {
+				zpath := joindot(binaryPath, "lzma")
+				if !conf.Runtime.DryRun {
+					ec.Push(mkdirdashp(zpath))
 				}
-				return nil
-			})
+				zbin := filepath.Join(zpath, binaryName)
+
+				upxcmd := basecmd.Clone().WithName("upx")
+				if build.GOOS == "darwin" {
+					upxcmd.WithArgs("--force-macos", "-q", "--lzma", binPath, "-o", zbin)
+				} else {
+					upxcmd.WithArgs("-q", "--lzma", binPath, "-o", zbin)
+				}
+				steps = append(steps,
+					upxcmd,
+					basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.lzma.sha256", binPath, filepath.Join(versionBuildPath, artifactName))),
+				)
+
+				if build.GOARCH == "386" {
+					bin32 := filepath.Join(versionBuildPath, joinstr(Name, "32"))
+					steps = append(steps,
+						basecmd.Clone().WithName("cp").WithArgs(zbin, bin32),
+						basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.sha256", bin32, bin32)),
+					)
+				}
+
+				if build.GOOS == "darwin" && build.GOARCH == "arm64" {
+					binapp := filepath.Join(versionBuildPath, joindot(Name, "app"))
+					steps = append(steps,
+						basecmd.Clone().WithName("cp").WithArgs(binPath, binapp),
+						basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.sha256", binapp, binapp)),
+					)
+				}
+			}
+
+			switch build.GOOS {
+			case "windows":
+				zipball := filepath.Join(versionBuildPath, joindot(artifactName, "zip"))
+				steps = append(steps,
+					basecmd.Clone().WithName("zip").WithArgs("-j", zipball, binPath),
+					basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.sha256", zipball, zipball)),
+				)
+			default:
+				tarball := filepath.Join(versionBuildPath, joindot(artifactName, "tar.gz"))
+				steps = append(steps,
+					basecmd.Clone().WithName("tar").WithArgs("czvf", tarball, "-C", binaryPath, binaryName),
+					basecmd.Clone().Shell("bash", fmt.Sprintf("sha256sum %s > %s.sha256", tarball, tarball)),
+				)
+			}
+
+			if conf.Runtime.DryRun {
+				for _, step := range steps {
+					grip.Info(grip.KV("cmd", step.Name).KV("args", step.Args).KV("cmd", step))
+				}
+				continue
+			} else {
+				jobs.PushBack(func(ctx context.Context) error {
+					for _, step := range steps {
+						if err := step.Run(ctx); err != nil {
+							return err
+						}
+					}
+					return nil
+				})
+			}
+
+		}
+		if conf.Runtime.DryRun {
+			return ec.Resolve()
 		}
 
-	}
-	if conf.Runtime.DryRun {
+		ec.Push(wpa.RunWithPool(jobs.IteratorFront(), wpa.WorkerGroupConfDefaults()).Run(ctx))
 		return ec.Resolve()
 	}
+	return ers.New("no odem checkout discoverable")
+}
 
-	ec.Push(wpa.RunWithPool(jobs.IteratorFront(), wpa.WorkerGroupConfDefaults()).Run(ctx))
-	return ec.Resolve()
+func basePathCandidates(conf *odem.Configuration) iter.Seq[string] {
+	pwd := erc.Must(os.Getwd())
+
+	return irt.Keep(irt.Args(pwd, conf.Build.LocalRepoPath, home.TryExpandDirectory("~/src/odem/")), fileExists)
 }
 
 func LocalBuild(ctx context.Context) error {
 	conf := odem.GetConfiguration(ctx)
-	pwd := erc.Must(os.Getwd())
 	curVersion := GitDescribe()
 
-	for basepath := range irt.Keep(irt.Args(pwd, conf.Build.LocalRepoPath, home.TryExpandDirectory("~/src/odem/")), fileExists) {
+	for basepath := range basePathCandidates(conf) {
 		path := filepath.Join(basepath, "cmd", "odem.go")
 		if !fileExists(path) {
 			continue
@@ -175,5 +185,6 @@ func LocalBuild(ctx context.Context) error {
 			WithArgs(args...).
 			Run(ctx)
 	}
-	return erc.Errorf("no odem checkout discoverable: %s", pwd)
+
+	return ers.New("no odem checkout discoverable")
 }
