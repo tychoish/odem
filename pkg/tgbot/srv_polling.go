d8463e237e4dc3ada298ec23ae993c58fabf8e7b
tycho <garen@tychoish.com>
2026-04-18 13:48:51 -0400

    fix: improve retries for polling service

--- pkg/tgbot/srv_polling.go
+++ pkg/tgbot/srv_polling.go
@@ -2,39 +2,49 @@ package tgbot
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	etron "github.com/NicoNex/echotron/v3"
 	"github.com/tychoish/fun/erc"
+	"github.com/tychoish/fun/ers"
 	"github.com/tychoish/fun/stw"
 	"github.com/tychoish/grip"
 )
 
 func (srv *Service) startPolling(ctx context.Context, dsp *etron.Dispatcher) error {
 	timer := time.NewTimer(0)
+	defer timer.Stop()
+
 	var count int
 	for {
 		count++
+		var apierr *etron.APIError
 		select {
 		case err := <-srv.doPoll(ctx, count, dsp):
-			if err != nil {
+			switch {
+			case err == nil:
+				grip.Debug(grip.KV("op", "longpoll").KV("status", "normal").KV("outcome", "continue"))
+			case ers.IsExpiredContext(err):
 				return err
+			case errors.As(err, &apierr):
+				grip.Info(grip.KV("op", "longpoll").KV("status", "api-error").KV("code", apierr.ErrorCode()).KV("desc", apierr.Description()).KV("outcome", "continue"))
+			case ers.IsTerminating(err):
+				grip.Alert(grip.KV("op", "longpoll").KV("status", "terminating").WithError(err))
+				return nil
+			default:
+				grip.Error(grip.KV("op", "longpoll").KV("status", "non-terminating").WithError(err).KV("outcome", "continue"))
+			}
+			timer.Reset(5 * time.Second)
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-timer.C:
 			}
 		case <-ctx.Done():
 			return ctx.Err()
 		}
 
-		if shouldExit, err := srv.checkServiceExit(ctx); shouldExit {
-			return err
-		}
-
-		timer.Reset(5 * time.Second)
-		select {
-		case <-timer.C:
-			continue
-		case <-ctx.Done():
-			return ctx.Err()
-		}
 	}
 }
 
